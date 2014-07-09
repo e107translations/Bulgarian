a5280b0d1970f08c8726f47011374959f5ab80ab
Veselin Totev <v.totev@clabteam.com>
2014-07-09 12:31:41 +0300

    Мета тагове (lan_meta.php)

--- e107_languages/Bulgarian/admin/lan_meta.php
+++ e107_languages/Bulgarian/admin/lan_meta.php
@@ -13,16 +13,6 @@
 */
 define("METLAN_00", "Мета тагове"); 
 
-define("METLAN_1", "Мета таговете са обновени");
-define("METLAN_2", "Напишете мета тагове");
-define("METLAN_3", "Запазване на настройките");
-// define("METLAN_4", "Обновено");
-define("METLAN_5", "въведете описанието тук");
-define("METLAN_6", "въведете, списък, от, своите, ключови, думи, тук");
-define("METLAN_7", "въведете своята информация за права за ползване на сайта тук");
-define("METLAN_8", "Мета тагове");
-define("METLAN_9", "описание");
-define("METLAN_10", "ключови думи");
-define("METLAN_11", "права за ползване на сайта");
-define("METLAN_12", "Използвай заглавие и обобщение от новини като мета описание на страниците на новини.");
-define("METLAN_13", "Автор");
+define("METLAN_1", "Допълнителни мета тагове");
+define("METLAN_2", "например < meta name='revisit-after' content='30 days' />"); //FIXME space between < and meta: parses meta tag for some reason
+define("METLAN_3", "Използвай заглавие и обобщение от новини като мета описание на страниците на новини.");
