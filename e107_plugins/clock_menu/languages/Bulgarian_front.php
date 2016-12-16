52eff7eb9dae0c9532e5b0779a6c1a37ff21f55b
veskoto <v.totev@corllete.com>
2016-12-16 10:29:04 +0200

    Typo

---
+++ e107_plugins/clock_menu/languages/Bulgarian_front.php
@@ -0,0 +1,38 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Bulgarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/12/14 07:14:15
+|
+|        $Author: veskoto $
++---------------------------------------------------------------+
+*/
+
+define("CLOCK_MENU_L1", "Clock menu configuration saved");
+define("CLOCK_MENU_L2", "Caption");
+define("CLOCK_MENU_L3", "Update Menu Settings");
+define("CLOCK_MENU_L4", "Clock Menu Config	");
+define("CLOCK_MENU_L5", "Понеделник,");
+define("CLOCK_MENU_L6", "Вторник,");
+define("CLOCK_MENU_L7", "Сряда,");
+define("CLOCK_MENU_L8", "Четвъртък,");
+define("CLOCK_MENU_L9", "Петък,");
+define("CLOCK_MENU_L10", "Събота,");
+define("CLOCK_MENU_L11", "Неделя");
+define("CLOCK_MENU_L12", "Януари");
+define("CLOCK_MENU_L13", "Февруари");
+define("CLOCK_MENU_L14", "Март");
+define("CLOCK_MENU_L15", "Април");
+define("CLOCK_MENU_L16", "Май");
+define("CLOCK_MENU_L17", "Юни");
+define("CLOCK_MENU_L18", "Юли");
+define("CLOCK_MENU_L19", "Август");
+define("CLOCK_MENU_L20", "Септември");
+define("CLOCK_MENU_L21", "Октомври");
+define("CLOCK_MENU_L22", "Ноември");
+define("CLOCK_MENU_L23", "Декември");
+
+
+?>
---
+++ e107_themes/bootstrap3/languages/Bulgarian_admin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * e107 website system - Bulgarian Translation
+ *
+ * Copyright (C) 2005-2016 e107 Bulgaria e107.bg
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Language File
+ *
+*/
+
+define("LAN_THEMEPREF_00", "Branding:");
+define("LAN_THEMEPREF_01", "Navbar Alignment:");
+define("LAN_THEMEPREF_02", "Signup/Login Placement:");
+define("LAN_THEMEPREF_03", "Bootswatch Styles:");
+define("LAN_THEMEPREF_04", "Site Name");
+define("LAN_THEMEPREF_05", "Logo");
+define("LAN_THEMEPREF_06", "Logo &amp; Site Name");
+define("LAN_THEMEPREF_07", "left");
+define("LAN_THEMEPREF_08", "right");
+define("LAN_THEMEPREF_09", "top");
+define("LAN_THEMEPREF_10", "bottom");
+ 
+
+?>
